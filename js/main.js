a2ec1d713b3095c6e51826f25f66b379beb2aa4c
meed ghoufrane <baloncesmeed@gmail.com>
2023-10-20 15:31:17 +0100

    add responsive and edit nav bar form mobil

---
+++ js/main.js
@@ -0,0 +1,8 @@
+const  navmobile = document.querySelector('.navmobile');
+const btnnav = document.querySelector('.burger-list');
+
+
+function showdropdownlist(){
+    navmobile.classList.toggle('is-active');
+};
+
