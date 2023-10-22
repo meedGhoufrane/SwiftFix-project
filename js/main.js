477770e10ffc68eb35b426fd1f356c4cc238ef42
meed ghoufrane <baloncesmeed@gmail.com>
2023-10-22 18:12:38 +0100

    create a functio for

--- js/main.js
+++ js/main.js
@@ -1,8 +1,73 @@
 const  navmobile = document.querySelector('.navmobile');
-const btnnav = document.querySelector('.burger-list');
 
 
 function showdropdownlist(){
     navmobile.classList.toggle('is-active');
 };
+
+
+// search button
+function showserchiputs(){
+    var inputserch = document.getElementById('input-srch');
+
+    if(inputserch.style.display === "none"){
+      inputserch.style.display = 'block';
+      inputserch.style.transition = ".2s linear"
+
+    }else{
+      inputserch.style.display = 'none';
+      inputserch.style.transition = '.2s linear';
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// validation
+
+
+
+
+
+ let myform = document.getElementById('from');
+
+  myform.addEventListener('submit', function(e){ 
+    
+    var errorlastname = document.querySelector('.lastname-error');
+    var errorgmail = document.querySelector('.gmail-error');
+    var errorldiscr = document.querySelector('.description-error');
+
+
+    let firstname = document.getElementById('first-name');
+    // let myregex = /^[a-zA-Z]+$/;
+
+    if(firstname.value.trim() === ""){
+        let errorfirstname = document.getElementById('firstnm-error');
+        errorfirstname.innerHTML = "first Nanme is required";
+        errorfirstname.style.color = 'red';
+        e.preventDefault();
+    }
+  });
+
+
+
+
+
 
